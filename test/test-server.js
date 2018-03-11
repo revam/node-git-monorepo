587b4194a4a97c86e6e84a0d5c256d93ce64dc54
Mikal Stordal <mikalstordal@gmail.com>
2018-03-11 15:10:09 +0100

    Update test/test-server.js

--- test/test-server.js
+++ test/test-server.js
@@ -1,61 +1,65 @@
-'use strict';
-// @ts-check
+"use strict";
 
+let counter = 0;
 const http = require("http");
 const HttpStatus = require("http-status");
-const { createDriver, Service, ServiceType } = require(".");
-
-const { ORIGIN_ENV: origin = "./repos" } = process.env;
-const driver = createDriver(origin);
+const { createDriver, createDriverCache, Service, ServiceType } = require("..");
+const { ORIGIN_ENV: origin = "./repos", PORT } = process.env;
+const port = safeParseInt(PORT, 3000);
+const cache = createDriverCache();
+const driver = createDriver(origin, cache);
 const server = http.createServer(async function(request, response) {
   if (request.url === "/favicon.ico") {
     response.statusCode = 404;
-    return resonse.end();
+    return response.end();
   }
 
-  console.log(`REQUEST - ${request.method} - ${request.url}`);
+  const id = counter++;
+  console.log(`${id} - REQUEST - ${request.method} - ${request.url}`);
+  response.on("finish", () => console.log(`${id} - RESPONSE - ${response.statusCode}`));
+
   const service = new Service(driver, request.method, request.url, request.headers, request);
 
   service.onAccept.addOnce(function ({status, headers, body}) {
-    headers.forEach(function (value, header) { response.setHeader(header, value) });
+    headers.forEach(function (value, header) { response.setHeader(header, value); });
     response.statusCode = status;
     body.pipe(response, {end: true});
   });
   service.onReject.addOnce(function ({status, headers, reason}) {
-    headers.forEach(function (value, header) { response.setHeader(header, value) });
+    headers.forEach(function (value, header) { response.setHeader(header, value); });
     response.statusCode = status;
     response.end(reason || HttpStatus[status], "utf8");
   });
-  service.onError.addOnce(function(err) {
-    console.error(err);
+  service.onError.addOnce(function (err) {
     if (!response.headersSent) {
       response.statusCode = err.status || err.statusCode || 500;
       response.setHeader("Content-Type", "text/plain");
       response.end(HttpStatus[response.statusCode], "utf8");
+    } else if (response.connection.writable) {
+      response.end();
     }
   });
+  service.onError.add(function (err) {
+    console.error(err, id);
+  });
 
-  console.log(`SERVICE - ${ServiceType[service.type]} - ${service.repository}`)
+  console.log(`${id} - SERVICE - ${ServiceType[service.type]} - ${service.repository}`)
 
-  try {
-    await service.ready;
-    if (!await service.exists()) {
-      await service.reject(404);
-    } else if (!await service.access()) {
-      await service.reject(403);
-    } else {
-      await service.accept();
-    }
-  } catch (err) {
-    console.log("WTF ERROR!");
-    service.onError.dispatch(err);
+  service.inform("Served from package 'git-service' found at npmjs.com");
+
+  if (!await service.exists()) {
+    await service.reject(404);
+  } else if (!await service.access()) {
+    await service.reject(403);
+  } else {
+    await service.accept();
   }
-
-  console.log(`RESPONSE - ${request.method} - ${request.url} - ${response.statusCode}\n`);
 });
 
-process.on('SIGTERM', async function() {
-  server.close();
-});
+process.on("SIGTERM", () => server.close());
+server.listen(port, () => console.log(`server is listening on port ${port}`));
 
-server.listen(3000, () => console.log("server is ready"));
+function safeParseInt(source, default_value) {
+  const value = parseInt(source);
+  return Number.isNaN(value) ? default_value : value;
+}

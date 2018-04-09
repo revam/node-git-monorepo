5ce51a57abe24be1ba30c38ef8d8f29d549585fb
Mikal <mikalstordal@gmail.com>
2018-04-09 20:22:58 +0200

    Wipe tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,3 @@
-// from packages
-import { ok } from 'assert';
-import { spawn } from 'child_process';
-import * as intoStream from 'into-stream';
-import { resolve } from 'path';
-import { Readable, Writable } from 'stream';
-import { directory } from 'tempy';
-import * as through from 'through';
-import { promisify } from 'util';
-// from libraries
-import { GitProxyCore, Headers, repositoryExists } from '../src';
-import { create_bare_repo, create_non_repo, create_repo } from './helpers';
-
-it('need rewrite', (done) => done());
+describe('Service', () => {
+  it('should need a rewrite', (done) => done());
+});

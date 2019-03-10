a062d6497babc73fc8132ac625b297f7539b7b53
Mikal Stordal <mikalstordal@gmail.com>
2019-03-10 16:19:03 +0100

    Update configuration and more
     ### Changed
    
    - Seperate build configuration from default configuration for
      typescript compiler. Include all typescipt files in default
      configuration, and don't emit any files. Build config includes
      only the files in the source folder, and comes in three variants;
      - Transpile sources and create declaration files.
      - Only transpile sources.
      - Only create declaration files.
    
    - Updated npm scripts to use the new configuration files.
    
    - Renamed npm scripts "tsc:lib" and "tsc:declaration" to
      "tsc:js" and "tsc:d.ts" respectively.
    
     ### Fixed
    
    - Removed option `resolveJsonModule` from file
      "rollup.config.js". It was not used and conflicting
      with the default configuration.

--- packages/git-core/rollup.config.js
+++ packages/git-core/rollup.config.js
@@ -1,1 +1,1 @@
-eval(require("typescript").transpile(require("fs").readFileSync("./rollup.config.ts").toString(), {...(require("./tsconfig.json") || {}).compilerOptions, module: "commonjs", resolveJsonModule: true }));
+eval(require("typescript").transpile(require("fs").readFileSync("./rollup.config.ts").toString(), {...(require("./tsconfig.json") || {}).compilerOptions, module: "commonjs" }));

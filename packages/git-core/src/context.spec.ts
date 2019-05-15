dd99bb37a13c97fd0096707a9e35b6c95384f1ad
Mikal Stordal <mikalstordal@gmail.com>
2019-05-15 18:59:55 +0200

    Add more tests for properties in Context

--- packages/git-core/src/context.spec.ts
+++ packages/git-core/src/context.spec.ts
@@ -1,5 +1,6 @@
 import { Headers } from "node-fetch";
 import * as lib from "./context";
+import * as pLib from "./context.private";
 import { Service } from "./enum";
 import { concat } from "./util/buffer";
 
@@ -7,14 +8,14 @@ type ClassTypeArgs<T extends new (...args: any[]) => any> = T extends new (...ar
 
 describe("class Context", () => {
 
+  /**
+   * Create an empty async iterable iterator.
+   */
+  async function* asyncIterable(): AsyncIterableIterator<Uint8Array> { return; }
+
   //#region constructor
 
-  describe("constructor()", () => {
-    /**
-     * Create an empty async iterable iterator.
-     */
-    async function* asyncIterable(): AsyncIterableIterator<Uint8Array> { return; }
-
+  describe("public constructor():", () => {
     /**
      * Expect arguments to not make the constructor throw, and the expecting
      * results to match the provided values.
@@ -1230,43 +1231,289 @@ describe("class Context", () => {
   //#region properties
   //#region instance properties
 
-  describe("public read-only property advertisement:", () => {
-    test("should ALWAYS be a boolean", () => {
-      const args: Array<ClassTypeArgs<typeof lib.Context>> = [
-        ["127.0.0.1", "/", "GET", { async *[Symbol.asyncIterator]() { return; }, }, {}, false],
-        ["127.0.0.1", "/", "GET", { async *[Symbol.asyncIterator]() { return; }, }, {}, true],
+  describe("public property advertisement:", () => {
+    test("should be a boolean", () => {
+      const args: Array<[ClassTypeArgs<typeof lib.Context>, boolean?]> = [
+        [["127.0.0.1", "/", "GET", asyncIterable(), {}, undefined]],
+
+        [[], false],
+        [["127.0.0.1", "/", "GET", asyncIterable(), {}], false],
+        [["127.0.0.1", "/", "GET", asyncIterable(), {}, false], false],
+
+        [["127.0.0.1", "/info/refs?service=git-upload-pack", "GET", asyncIterable(), {}], true],
+        [["127.0.0.1", "/", "GET", asyncIterable(), {}, true], true],
       ];
-      for (const arg of args) {
-        const context = new lib.Context(...arg);
-        expect(context).toBeInstanceOf(lib.Context);
-        expect(typeof context.advertisement).toBe("boolean");
+      for (const [arg, result] of args) {
+        // Expect to throw
+        if (result === undefined) {
+          expect(() => new lib.Context(...arg)).toThrow();
+        }
+        // Expect result
+        else {
+          const context = new lib.Context(...arg);
+          expect(context).toBeInstanceOf(lib.Context);
+          expect(typeof context.advertisement).toBe("boolean");
+          expect(context.advertisement).toBe(result);
+        }
       }
+    });
+
+    test("should be non-writable", () => {
+      const context = new lib.Context();
+      expect(context).toBeInstanceOf(lib.Context);
+      expect(context.advertisement).toBe(false);
+      // tslint:disable-next-line
+      // @ts-ignore
+      expect(() => context.advertisement = true).toThrow();
+      expect(context.advertisement).toBe(false);
     });
   });
 
   describe("public property body:", () => {
-    test("should set Context.response.body", () => {
-      const uniSym: any = Symbol("unique symbol");
+    // Test statement using symbols
+    test("should be the same as Context.response.body", () => {
       const context = new lib.Context();
       expect(context).toBeInstanceOf(lib.Context);
-      context.body = uniSym;
-      expect(context.response.body).toBe(uniSym);
-    });
-
-    test("should get Context.response.body", () => {
-      const uniSym: any = Symbol("unique symbol");
-      const context = new lib.Context();
-      expect(context).toBeInstanceOf(lib.Context);
-      context.response.body = uniSym;
-      expect(context.body).toBe(uniSym);
+      expect(context.body).toBe(context.response.body);
+      const uniSym1: any = Symbol("unique symbol");
+      context.body = uniSym1;
+      expect(context.body).toBe(context.response.body);
+      const uniSym2: any = Symbol("unique symbol");
+      context.response.body = uniSym2;
+      expect(context.response.body).toBe(context.body);
     });
   });
 
   describe("public property headers:", () => {
-    test("should be same node-fetch#Headers instance as Context.response.headers", () => {
+    test("should be an instance of node-fetch#Headers", () => {
+      const context = new lib.Context();
+      expect(context).toBeInstanceOf(lib.Context);
+      expect(context.headers).toBeInstanceOf(Headers);
+    });
+
+    // Test statement using symbols
+    test("should be the same as Context.response.headers", () => {
       const context = new lib.Context();
       expect(context).toBeInstanceOf(lib.Context);
       expect(context.headers).toBe(context.response.headers);
+      const uniSym1: any = Symbol("unique symbol");
+      context.headers = uniSym1;
+      expect(context.headers).toBe(context.response.headers);
+      const uniSym2: any = Symbol("unique symbol");
+      context.response.headers = uniSym2;
+      expect(context.response.headers).toBe(context.headers);
+    });
+  });
+
+  describe("public property ip:", () => {
+    test("should be the same as Context.request.ip", () => {
+      const ctx1 = new lib.Context("127.0.0.1");
+      expect(ctx1).toBeInstanceOf(lib.Context);
+      expect(ctx1.ip).toBe("127.0.0.1");
+      expect(ctx1.request.ip).toBe("127.0.0.1");
+      expect(ctx1.ip).toBe(ctx1.request.ip);
+
+      const ctx2 = new lib.Context("10.0.0.1");
+      expect(ctx2).toBeInstanceOf(lib.Context);
+      expect(ctx2.ip).toBe("10.0.0.1");
+      expect(ctx2.request.ip).toBe("10.0.0.1");
+      expect(ctx2.ip).toBe(ctx2.request.ip);
+    });
+
+    test("should be non-writable", () => {
+      const ctx1 = new lib.Context("127.0.0.1");
+      expect(ctx1).toBeInstanceOf(lib.Context);
+      expect(ctx1.ip).toBe("127.0.0.1");
+      // tslint:disable-next-line
+      // @ts-ignore
+      expect(() => ctx1.ip = "::1").toThrow();
+      expect(ctx1.ip).toBe("127.0.0.1");
+
+      const ctx2 = new lib.Context("10.0.0.1");
+      expect(ctx2).toBeInstanceOf(lib.Context);
+      expect(ctx2.ip).toBe("10.0.0.1");
+      // tslint:disable-next-line
+      // @ts-ignore
+      expect(() => ctx2.ip = "::1").toThrow();
+      expect(ctx2.ip).toBe("10.0.0.1");
+    });
+  });
+
+  describe("public property isInitialised:", () => {
+    test("should only be false when advertisement is false, service is set, and body is still being parsed/read/analysed", () => {
+      // Missing both
+      const ctx1 = new lib.Context("127.0.0.1", "/", "POST", asyncIterable(), {}, true, undefined, undefined);
+      expect(ctx1).toBeInstanceOf(lib.Context);
+      expect(ctx1.isInitialised).toBe(true);
+
+      // Missing (non-)advertisement
+      const ctx2 = new lib.Context("127.0.0.1", "/", "POST", asyncIterable(), {}, true, undefined, Service.UploadPack);
+      expect(ctx2).toBeInstanceOf(lib.Context);
+      expect(ctx2.isInitialised).toBe(true);
+
+      // Missing service
+      const ctx3 = new lib.Context("127.0.0.1", "/", "POST", asyncIterable(), {}, false, undefined, undefined);
+      expect(ctx3).toBeInstanceOf(lib.Context);
+      expect(ctx3.isInitialised).toBe(true);
+
+      // Meet the requirements
+      const ctx4 = new lib.Context("127.0.0.1", "/", "POST", asyncIterable(), {}, false, undefined, Service.UploadPack);
+      expect(ctx4).toBeInstanceOf(lib.Context);
+      expect(ctx4.isInitialised).toBe(false);
+    });
+
+    test("should be non-writable", () => {
+      const context = new lib.Context();
+      expect(context).toBeInstanceOf(lib.Context);
+      expect(context.isInitialised).toBe(true);
+      // tslint:disable-next-line
+      // @ts-ignore
+      expect(() => context.isInitialised = false).toThrow();
+      expect(context.isInitialised).toBe(true);
+    });
+  });
+
+  describe("public property method:", () => {
+    test("should be the same as Context.request.method", () => {
+      const ctx1 = new lib.Context("127.0.0.1", "/", "GET");
+      expect(ctx1).toBeInstanceOf(lib.Context);
+      expect(ctx1.method).toBe("GET");
+      expect(ctx1.request.method).toBe("GET");
+      expect(ctx1.method).toBe(ctx1.request.method);
+
+      const ctx2 = new lib.Context("127.0.0.1", "/", "POST");
+      expect(ctx2).toBeInstanceOf(lib.Context);
+      expect(ctx2.method).toBe("POST");
+      expect(ctx2.request.method).toBe("POST");
+      expect(ctx2.method).toBe(ctx2.request.method);
+    });
+
+    test("should be non-writable", () => {
+      const ctx1 = new lib.Context("127.0.0.1", "/", "GET");
+      expect(ctx1).toBeInstanceOf(lib.Context);
+      expect(ctx1.method).toBe("GET");
+      // tslint:disable-next-line
+      // @ts-ignore
+      expect(() => ctx1.method = "PATCH").toThrow();
+      expect(ctx1.method).toBe("GET");
+
+      const ctx2 = new lib.Context("127.0.0.1", "/", "POST");
+      expect(ctx2).toBeInstanceOf(lib.Context);
+      expect(ctx2.method).toBe("POST");
+      // tslint:disable-next-line
+      // @ts-ignore
+      expect(() => ctx2.method = "PATCH").toThrow();
+      expect(ctx2.method).toBe("POST");
+    });
+  });
+
+  describe("public property pathname:", () => {
+    test("should default to a string, regardless of constructor input for value", () => {
+      // Infer pathname from url and method
+
+      const url1 = "/info/refs?service=git-upload-pack";
+      const [, pathname1] = pLib.inferValues(url1, "GET");
+      const ctx1 = new lib.Context("127.0.0.1", url1, "GET", asyncIterable());
+      expect(ctx1).toBeInstanceOf(lib.Context);
+      expect(typeof pathname1).toBe("undefined");
+      expect(typeof ctx1.pathname).toBe("string");
+      expect(pathname1).toBeUndefined();
+      expect(ctx1.pathname).toBe("");
+
+      const url2 = "/path/to/repo/info/refs?service=git-upload-pack";
+      const [, pathname2] = pLib.inferValues(url2, "GET");
+      const ctx2 = new lib.Context("127.0.0.1", url2, "GET", asyncIterable());
+      expect(ctx2).toBeInstanceOf(lib.Context);
+      expect(typeof pathname2).toBe("string");
+      expect(typeof ctx2.pathname).toBe("string");
+      expect(pathname2).toBe("path/to/repo");
+      expect(ctx2.pathname).toBe("path/to/repo");
+
+      // Set pathname in constructor
+
+      const ctx3 = new lib.Context("127.0.0.1", "/", "GET", asyncIterable(), {}, false, undefined);
+      expect(ctx3).toBeInstanceOf(lib.Context);
+      expect(typeof ctx3.pathname).toBe("string");
+      expect(ctx3.pathname).toBe("");
+
+      const ctx4 = new lib.Context("127.0.0.1", "/", "GET", asyncIterable(), {}, false, "");
+      expect(ctx4).toBeInstanceOf(lib.Context);
+      expect(typeof ctx4.pathname).toBe("string");
+      expect(ctx4.pathname).toBe("");
+
+      const ctx5 = new lib.Context("127.0.0.1", "/", "GET", asyncIterable(), {}, false, "path/to/repo");
+      expect(ctx5).toBeInstanceOf(lib.Context);
+      expect(typeof ctx5.pathname).toBe("string");
+      expect(ctx5.pathname).toBe("path/to/repo");
+    });
+
+    describe("public property url:", () => {
+      test("should be the same as Context.request.url", () => {
+        const ctx1 = new lib.Context("127.0.0.1", "/");
+        expect(ctx1).toBeInstanceOf(lib.Context);
+        expect(ctx1.url).toBe("/");
+        expect(ctx1.request.url).toBe("/");
+        expect(ctx1.url).toBe(ctx1.request.url);
+
+        const ctx2 = new lib.Context("127.0.0.1", "/path/to/some/repo?withAQuery=true");
+        expect(ctx2).toBeInstanceOf(lib.Context);
+        expect(ctx2.url).toBe("/path/to/some/repo?withAQuery=true");
+        expect(ctx2.request.url).toBe("/path/to/some/repo?withAQuery=true");
+        expect(ctx2.url).toBe(ctx2.request.url);
+      });
+
+      test("should be non-writable", () => {
+        const ctx1 = new lib.Context("127.0.0.1", "/");
+        expect(ctx1).toBeInstanceOf(lib.Context);
+        expect(ctx1.url).toBe("/");
+        // tslint:disable-next-line
+        // @ts-ignore
+        expect(() => ctx1.url = "/some/other/path?hidden=false").toThrow();
+        expect(ctx1.url).toBe("/");
+
+        const ctx2 = new lib.Context("127.0.0.1", "/path/to/some/repo?withAQuery=true");
+        expect(ctx2).toBeInstanceOf(lib.Context);
+        expect(ctx2.url).toBe("/path/to/some/repo?withAQuery=true");
+        // tslint:disable-next-line
+        // @ts-ignore
+        expect(() => ctx2.url = "/some/other/path?hidden=false").toThrow();
+        expect(ctx2.url).toBe("/path/to/some/repo?withAQuery=true");
+      });
+    });
+  });
+
+  describe("public read-only property readable:", () => {
+    test("should be non-writable", () => {
+      const context = new lib.Context();
+      expect(context).toBeInstanceOf(lib.Context);
+      // tslint:disable-next-line
+      // @ts-ignore
+      expect(() => context.readable = undefined).toThrow();
+    });
+
+    describe("request():", () => undefined);
+
+    describe("response():", () => undefined);
+  });
+
+  describe("public property status:", () => {
+    test("should default to 404 for new instances", () => {
+      const context = new lib.Context();
+      expect(context).toBeInstanceOf(lib.Context);
+      expect(context.status).toBe(404);
+    });
+
+    // Test statement using symbols
+    test("should be the same as Context.response.status", () => {
+      const context = new lib.Context();
+      expect(context).toBeInstanceOf(lib.Context);
+      expect(context.status).toBe(context.response.status);
+      const uniSym1: any = Symbol("unique symbol");
+      context.status = uniSym1;
+      expect(context.status).toBe(context.response.status);
+      const uniSym2: any = Symbol("unique symbol");
+      context.response.status = uniSym2;
+      expect(context.response.status).toBe(context.status);
     });
   });
 
@@ -1290,38 +1537,96 @@ describe("class Context", () => {
       return ctx;
     }
 
-    const Bodies: lib.Body[] = [
-      undefined,
-      null,
-      new Uint8Array(0),
-      new Uint8Array([48, 48, 48, 48]),
-      Promise.resolve(new Uint8Array(0)),
-      Promise.resolve(new Uint8Array([48, 48, 48, 48])),
-      { async then(resolve, reject) { return Promise.resolve(new Uint8Array(0)).then(resolve, reject); } },
-      { async then(resolve, reject) { return Promise.resolve(new Uint8Array([48, 48, 48, 48])).then(resolve, reject); } },
-      { [Symbol.iterator](): Iterator<Uint8Array> { return (function*() { yield new Uint8Array(0); })(); } },
-      { [Symbol.iterator](): Iterator<Uint8Array> { return (function*() { yield new Uint8Array([48, 48, 48, 48]); })(); } },
-      { *[Symbol.iterator](): IterableIterator<Uint8Array> { yield new Uint8Array(0); } },
-      { *[Symbol.iterator](): IterableIterator<Uint8Array> { yield new Uint8Array([48, 48, 48, 48]); } },
-      (function *(): IterableIterator<Uint8Array> { yield new Uint8Array(0); })(),
-      (function *(): IterableIterator<Uint8Array> { yield new Uint8Array([48, 48, 48, 48]); })(),
-      { [Symbol.asyncIterator](): AsyncIterator<Uint8Array> { return (async function*() { yield new Uint8Array(0); })(); } },
-      { [Symbol.asyncIterator](): AsyncIterator<Uint8Array> { return (async function*() { yield new Uint8Array([48, 48, 48, 48]); })(); } },
-      { async *[Symbol.asyncIterator](): AsyncIterableIterator<Uint8Array> { yield new Uint8Array(0); } },
-      { async *[Symbol.asyncIterator](): AsyncIterableIterator<Uint8Array> { yield new Uint8Array([48, 48, 48, 48]); } },
-    ];
+    function Bodies(): Array<[lib.Body, Uint8Array?]> {
+      return [
+        [
+          undefined,
+          undefined,
+        ],
+        [
+          null,
+          undefined,
+        ],
+        [
+          new Uint8Array(0),
+          new Uint8Array(0),
+        ],
+        [
+          new Uint8Array([48, 48, 48, 48]),
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+        [
+          Promise.resolve(new Uint8Array(0)),
+          new Uint8Array(0),
+        ],
+        [
+          Promise.resolve(new Uint8Array([48, 48, 48, 48])),
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+        [
+          { async then(resolve, reject) { return Promise.resolve(new Uint8Array(0)).then(resolve, reject); } },
+          new Uint8Array(0),
+        ],
+        [
+          { async then(resolve, reject) { return Promise.resolve(new Uint8Array([48, 48, 48, 48])).then(resolve, reject); } },
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+        [
+          { [Symbol.iterator](): Iterator<Uint8Array> { return (function*() { yield new Uint8Array(0); })(); } },
+          new Uint8Array(0),
+        ],
+        [
+          { [Symbol.iterator](): Iterator<Uint8Array> { return (function*() { yield new Uint8Array([48, 48, 48, 48]); })(); } },
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+        [
+          { *[Symbol.iterator](): IterableIterator<Uint8Array> { yield new Uint8Array(0); } },
+          new Uint8Array(0),
+        ],
+        [
+          { *[Symbol.iterator](): IterableIterator<Uint8Array> { yield new Uint8Array([48, 48, 48, 48]); } },
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+        [
+          (function *(): IterableIterator<Uint8Array> { yield new Uint8Array(0); })(),
+          new Uint8Array(0),
+        ],
+        [
+          (function *(): IterableIterator<Uint8Array> { yield new Uint8Array([48, 48, 48, 48]); })(),
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+        [
+          { [Symbol.asyncIterator](): AsyncIterator<Uint8Array> { return (async function*() { yield new Uint8Array(0); })(); } },
+          new Uint8Array(0),
+        ],
+        [
+          { [Symbol.asyncIterator]() { return (async function*() { yield new Uint8Array([48, 48, 48, 48]); })(); } },
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+        [
+          { async *[Symbol.asyncIterator](): AsyncIterableIterator<Uint8Array> { yield new Uint8Array(0); } },
+          new Uint8Array(0),
+        ],
+        [
+          { async *[Symbol.asyncIterator](): AsyncIterableIterator<Uint8Array> { yield new Uint8Array([48, 48, 48, 48]); } },
+          new Uint8Array([48, 48, 48, 48]),
+        ],
+      ];
+}
 
     const ReusableBodies: lib.Body[] = [
       (async function *(): AsyncIterableIterator<Uint8Array> { yield new Uint8Array(0); })(),
       (async function *(): AsyncIterableIterator<Uint8Array> { yield new Uint8Array([48, 48, 48, 48]); })(),
     ];
 
-    test("should return an async iterable iterator created from Context.body", () => {
-      for (const initBody of Bodies) {
+    test("should return an async iterable iterator created from Context.body", async () => {
+      for (const [initBody, expected] of Bodies()) {
         const context = createContext(initBody);
         const body = context.toAsyncIterator();
         expect(initBody).not.toBe(body);
         expect(context.toAsyncIterator()).toBe(body);
+        const {value: actual} = await body.next();
+        expect(actual).toEqual(expected);
       }
       for (const initBody of ReusableBodies) {
         const context = createContext(initBody);
@@ -1331,8 +1636,8 @@ describe("class Context", () => {
       }
     });
 
-    test("should set Context.body to return value", () => {
-      for (const initBody of Bodies) {
+    test("should set Context.body to return value of method", () => {
+      for (const [initBody] of Bodies()) {
         const context = createContext(initBody);
         const body = context.toAsyncIterator();
         expect(initBody).not.toBe(body);
@@ -1384,13 +1689,4 @@ describe("class Context", () => {
 
   //#endregion static methods
   //#endregion methods
-  //#region stream compatibility
-
-  describe("readable", () => {
-    describe("request():", () => undefined);
-
-    describe("response():", () => undefined);
-  });
-
-  //#endregion stream compatibility
 });
